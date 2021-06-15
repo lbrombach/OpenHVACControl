20d5148d9ee8ed10615cabc9b3fb6afdeca44616
Lloyd Brombach <lbrombach2@gmail.com>
2021-06-15 11:35:29 -0400

    initial commit on this branch

--- src/main/java/org/OpenHVACControl/Main.java
+++ src/main/java/org/OpenHVACControl/Main.java
@@ -3,6 +3,20 @@ package org.OpenHVACControl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+
+
+
+	//HTTP port
+//	@Value("${http.port}")
+//	private int httpPort;
+    
+    
 
 @SpringBootApplication
 public class Main {
@@ -10,4 +24,19 @@ public class Main {
         SpringApplication.run(Main.class, args);
         new MainController().mainController();
     }
+    
+	// Let's configure additional connector to enable support for both HTTP and HTTPS
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(8080);
+		return connector;
+	}
+    
 }
